38bc1729bab8fda958be38066a79ea67f4466f5c
ahmedosama <ahmedosamabedawy@gmail.com>
2025-03-05 16:00:15 +0200

    feat: :sparkles: Add Tailwind CSS and configure project structure

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes, withComponentInputBinding()),
+    provideAnimationsAsync(),
+    importProvidersFrom(BrowserModule),
+    importProvidersFrom(BrowserAnimationsModule),
+  ]
 };
---
+++ tailwind.config.js
@@ -0,0 +1,63 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '1rem !important',
+    },
+    opacity: {
+      "0-pct": "0%",
+      "5-pct": "5%",
+      "6-pct": "6%",
+      "10-pct": "10%",
+      "15-pct": "15%",
+      "16-pct": "16%",
+      "20-pct": "20%",
+      "25-pct": "25%",
+      "30-pct": "30%",
+      "35-pct": "35%",
+      "40-pct": "40%",
+      "45-pct": "45%",
+      "50-pct": "50%",
+      "55-pct": "55%",
+      "60-pct": "60%",
+      "65-pct": "65%",
+      "70-pct": "70%",
+      "75-pct": "75%",
+      "80-pct": "80%",
+      "85-pct": "85%",
+      "90-pct": "90%",
+      "95-pct": "95%",
+      "100-pct": "100%"
+    },
+    colors: {
+      ...colors,
+    },
+    extend: {
+      colors: {
+        primary: '#6941C6',
+        secondary: '#42307D',
+        cardinal: '#C1272D',
+        mangoOrange: '#F7863F',
+        porcelain: '#EDF1F7',
+        lavenderMist: '#e4e9f2',
+        mistBlue: "#667085",
+        davyGrey: '#54585C',
+        ebonyClay: '#101828',
+        santaGray: '#99a5af'
+      },
+
+      boxShadow: {
+        'lg': '0px 12px 16px -4px #10182814'
+      },
+    },
+  },
+  plugins: [],
+}
